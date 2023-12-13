616223f48f0c7068c146089efe8f281f6a32045a
RolandBakunts <rolandbakunts@gmail.com>
2023-12-13 23:38:42 +0400

    implemented terragrunt

--- main.tf
+++ main.tf
@@ -1,22 +1,23 @@
-module "networking" {
-  source = "./modules/networking"
-}
 
-module "instance" {
-  source       = "./modules/instance"
-  ssh_key      = module.ssh.public_key_openssh
-  user         = var.instance_user
-  instance_ami = var.instance_ami
-  ec2_sg_id    = module.networking.ec2_sg_id
-}
+# module "networking" {
+#   source = "./modules/networking"
+# }
 
-module "application" {
-  source = "./modules/application"
-  image  = var.image
-  host   = "ssh://${var.instance_user}@${module.instance.public_ip}:22"
-}
+# module "instance" {
+#   source       = "./modules/instance"
+#   ssh_key      = module.ssh.public_key_openssh
+#   user         = var.instance_user
+#   instance_ami = var.instance_ami
+#   ec2_sg_id    = module.networking.ec2_sg_id
+# }
 
-module "ssh" {
-  source = "./modules/ssh"
-  env    = var.env
-}
+# module "application" {
+#   source = "./modules/application"
+#   image  = var.image
+#   host   = "ssh://${var.instance_user}@${module.instance.public_ip}:22"
+# }
+
+# module "ssh" {
+#   source = "./modules/ssh"
+#   env    = var.env
+# }
