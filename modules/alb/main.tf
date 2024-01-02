7f11776331a1a35aef7186403db278f658ecaeb7
RolandBakunts <rolandbakunts@gmail.com>
2024-01-03 01:42:35 +0400

    delete unused code

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -37,17 +37,6 @@ resource "aws_alb_target_group" "main" {
   }
 }
 
-# resource "aws_alb_listener" "http" {
-#   load_balancer_arn = aws_lb.main.id
-#   port              = 80
-#   protocol          = "HTTP"
-
-#   default_action {
-#     target_group_arn = aws_alb_target_group.main.id
-#     type             = "forward"
-#   }
-# }
-
 # Redirect to https listener
 resource "aws_alb_listener" "http" {
   load_balancer_arn = aws_lb.main.id
